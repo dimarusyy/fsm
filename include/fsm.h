7b56ebcc70a4a1608658e1890f3484bd5418a454
Dima Rusyy <dima.ru.com@gmail.com>
2021-06-03 10:13:17 +0300

    Add project files.

---
+++ include/fsm.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <array>
+#include <unordered_map>
+#include <functional>
+#include <optional>
+
+namespace fsm
+{
+    template <typename T>
+    using states_t = std::array<T, 26>;
+
+    using callback_t = std::function<void()>;
+
+    struct state_t
+    {
+        char state;
+        callback_t handler;
+    };
+
+    struct transition_t
+    {
+        bool enabled = false;
+        callback_t fallback;
+
+        std::unordered_map<std::string /*input*/, state_t> next;
+    };
+
+    struct fsm
+    {
+        static constexpr char unknown_state = ' ';
+
+        void AddState(char state)
+        {
+            at(state).enabled = true;
+        }
+
+        void AddTransition(char from, char to, std::string input, callback_t handler)
+        {
+            at(from).next[input] = state_t{to, handler};
+        }
+
+        void AddFallback(char from, callback_t handler)
+        {
+            at(from).fallback = handler;
+        }
+
+        void ResetMachine(char state)
+        {
+            // don't change state if not enabled
+            if(at(state).enabled)
+                _current_state = state;
+        }
+
+        void Step(std::string input)
+        {
+            auto &state = at(_current_state.value());
+
+            auto next_state_it = state.next.find(input);
+            if(next_state_it != state.next.end())
+            {
+                _current_state = next_state_it->second.state;
+                if(next_state_it->second.handler)
+                    next_state_it->second.handler();
+            }
+            else
+            {
+                // fallback
+                if(state.fallback)
+                    state.fallback();
+            }
+        }
+
+        char GetState() const
+        {
+            return _current_state.value();
+        }
+
+    protected:
+        transition_t& at(char state)
+        {
+            return _states[state - 'a'];
+        }
+
+    private:
+        std::optional<char> _current_state{ unknown_state };
+        states_t<transition_t> _states;
+    };
+}
---
+++ tests/fsm.tests.cpp
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+#include "fsm.h"
+
+TEST(FSM, ExampleFromSpec)
+{
+    bool invoked = false;
+    fsm::fsm m;
+    m.AddState('a');
+    m.AddState('b');
+    m.AddTransition('a', 'b', "input1", [&]()
+                    { invoked = true; });
+
+    m.ResetMachine('a');
+    m.Step("input1");
+
+    EXPECT_EQ(m.GetState(), 'b');
+    EXPECT_TRUE(invoked);
+
+    m.AddState('c');
+    m.AddTransition('b', 'c', "input2", {});
+    m.Step("input2");
+    EXPECT_EQ(m.GetState(), 'c');
+}
+
+TEST(FSM, FallbackInvoked)
+{
+    bool invoked = false;
+    fsm::fsm m;
+    m.AddState('a');
+    m.AddState('b');
+    m.AddTransition('a', 'b', "input1",{});
+    m.AddFallback('a', [&]() { invoked = true; });
+
+    m.ResetMachine('a');
+    m.Step("input");
+
+    EXPECT_TRUE(invoked);
+}
+
+TEST(FSM, TransitionWrongInputNoFallback)
+{
+    fsm::fsm m;
+    m.AddState('a');
+    m.AddState('b');
+    m.AddTransition('a', 'b', "input1", {});
+
+    m.ResetMachine('a');
+    m.Step("input");
+    EXPECT_EQ(m.GetState(), 'a');
+}
+
+TEST(FSM, ResetStateOnce)
+{
+    fsm::fsm m;
+    m.AddState('a');
+
+    m.ResetMachine('a');
+    EXPECT_EQ(m.GetState(), 'a');
+}
+
+TEST(FSM, ResetStateTwice)
+{
+    fsm::fsm m;
+    m.AddState('a');
+    m.AddState('b');
+
+    m.ResetMachine('a');
+    m.ResetMachine('b');
+    EXPECT_EQ(m.GetState(), 'b');
+}
+
+TEST(FSM, ResetStateNoState)
+{
+    fsm::fsm m;
+    m.ResetMachine('a');
+    EXPECT_EQ(m.GetState(), fsm::fsm::unknown_state);
+}
